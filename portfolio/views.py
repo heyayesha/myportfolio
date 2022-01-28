06af4f8b11d2bcf824b6bc1eb4c3cec05da1f564
heyayesha <92508667+heyayesha@users.noreply.github.com>
2022-01-27 21:07:52 -0500

    edited

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,4 +6,4 @@ from .models import Project
 def home(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'projects': projects})
-# Create your views here.
+
